a091b2a97a13615e14da06041905ea24b0a666d2
N4nid <136599840+N4nid@users.noreply.github.com>
2023-12-25 12:00:59 +0100

    added button for test notification
    now able to send notifications

--- app/src/main/java/dev/nanid/selfcare/MainActivity.kt
+++ app/src/main/java/dev/nanid/selfcare/MainActivity.kt
@@ -1,8 +1,21 @@
 package dev.nanid.selfcare
 
+import android.Manifest
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.widget.Button
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -13,6 +26,11 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val channelId = "nanid.selfcare.notifications"
+    private val description = "Test notification"
+    lateinit var notificationManager: NotificationManagerCompat
+    lateinit var notificationChannel: NotificationChannel
+    lateinit var builder: Notification.Builder
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -28,5 +46,46 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        //notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val btn = findViewById<Button>(R.id.button)
+        btn.setOnClickListener {
+            btn.text = "wowi"
+            notifyIn()
+        }
     }
+    //----------- own shit from here ----------
+    fun notifyIn(){
+        if (Build.VERSION.SDK_INT >= 26) {
+            val channel = NotificationChannel(channelId , "Your channel name", NotificationManager.IMPORTANCE_DEFAULT)
+            NotificationManagerCompat.from(this).createNotificationChannel(channel)
+        }
+        var builder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("My notification")
+            .setContentText("Much longer text that cannot fit one line...")
+            .setStyle(NotificationCompat.BigTextStyle()
+                .bigText("Much longer text that cannot fit one line..."))
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            val btn = findViewById<Button>(R.id.button)
+            btn.text = ":c"
+            return
+        }
+        NotificationManagerCompat.from(this).notify(1234, builder.build())
+    }
+
 }
+
--- app/src/main/java/dev/nanid/selfcare/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/dev/nanid/selfcare/ui/notifications/NotificationsFragment.kt
@@ -4,15 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import dev.nanid.selfcare.MainActivity
 import dev.nanid.selfcare.databinding.FragmentNotificationsBinding
 
 class NotificationsFragment : Fragment() {
 
     private var _binding: FragmentNotificationsBinding? = null
-
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
@@ -32,6 +31,7 @@ class NotificationsFragment : Fragment() {
     }
 
     override fun onDestroyView() {
+
         super.onDestroyView()
         _binding = null
     }
